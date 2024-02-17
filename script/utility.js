90685808792e29800abc28591e39cb26cb56186c
MaskuraBegum <maskura.begum77@gmail.com>
2024-02-17 14:00:50 +0600

    fullfiled the game

--- script/utility.js
+++ script/utility.js
@@ -8,6 +8,7 @@ function showAnElement(showFiled){
 }
 function play(){
     hideAnElement('home');
+    hideAnElement('score')
     showAnElement('playground');
     loopGame();
 }
@@ -28,3 +29,45 @@ function setBackground(alpha){
     color = document.getElementById(alpha);
     color.classList.add('bg-orange-400');
 }
+function removeBackground(alpha){
+    removeColor = document.getElementById(alpha);
+    removeColor.classList.remove('bg-orange-400');
+}
+function keypressHandle(event){
+    const gamer = event.key;
+    console.log(gamer);
+    const expected = document.getElementById('the-alphabet');
+    if(expected.innerText == gamer){
+        currentScore = document.getElementById('current-score');
+        toInt = parseInt(currentScore.innerText);
+        score = toInt + 1;
+        currentScore.innerText = score;
+
+        removeBackground(expected.innerText);
+        loopGame();
+    }
+    else{
+        currentLife = document.getElementById('current-life')
+        toIntLife = parseInt(currentLife.innerText);
+        life = toIntLife -1;
+        currentLife.innerText = life;
+        if(life == 0){
+            hideAnElement('playground')
+            showAnElement('score')
+            currentScore = document.getElementById('current-score');
+            finalScore = document.getElementById('final-score');
+            finalScore.innerText = currentScore.innerText;
+            currentScore = document.getElementById('current-score');
+            currentScore.innerText = '0';
+            currentLife = document.getElementById('current-life');
+            currentLife.innerText = '3';
+
+            lastalpha = document.getElementById('the-alphabet')
+            removeBackground(lastalpha.innerText);
+            score = 0;
+        }
+
+    }
+}
+document.addEventListener('keyup', keypressHandle)
+
